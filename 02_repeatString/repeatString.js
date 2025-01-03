7b1e5e3a81838ea9f11503b047a12b5dcbeea18c
mrHock89 <147297458+mrHock89@users.noreply.github.com>
2025-01-03 11:27:24 +0530

    solved exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
-
+const repeatString = function (myString, times) {
+  if (times < 0) return "ERROR";
+  let string = "";
+  for (let i = 0; i < times; i++) {
+    string += myString;
+  }
+  return string;
 };
 
+// const repeatString = function (word, times) {
+//   if (times < 0) return "ERROR";
+//   let string = "";
+//   for (let i = 0; i < times; i++) {
+//     string += word;
+//   }
+//   return string;
+// };
 // Do not edit below this line
 module.exports = repeatString;
