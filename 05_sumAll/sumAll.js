e409ccf1efbc019a5a5e50117b61c90cbc4ad08f
mrHock89 <147297458+mrHock89@users.noreply.github.com>
2025-01-03 17:06:17 +0530

    solved exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function (startNum, endNum) {
+  // chcking is the are number or not
+  if (!Number.isInteger(startNum) || !Number.isInteger(endNum)) return "ERROR";
+  // checking are the positive
+  if (startNum < 0 || endNum < 0) return "ERROR";
+  // Swaping number if start number is bigger than endNumber
+  //swaping the numbers
+  if (startNum > endNum) {
+    let temp = startNum;
+    startNum = endNum;
+    endNum = temp;
+  }
 
+  // NOw if all the previous condition is true returning the sum
+  let sum = 0;
+  for (startNum; startNum <= endNum; startNum++) {
+    sum += startNum;
+  }
+  return sum;
 };
 
 // Do not edit below this line
