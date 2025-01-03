6899a3b72612bf2c4c5e8ce200c89cb3ff5909ed
mrHock89 <147297458+mrHock89@users.noreply.github.com>
2025-01-03 16:40:08 +0530

    solves exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...num) {
+  const myArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (!num.includes(arr[i])) {
+      myArr.push(arr[i]);
+    }
+  }
+  return myArr;
 };
 
 // Do not edit below this line
