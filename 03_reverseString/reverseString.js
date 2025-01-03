7117b591b95d541cb1e560d92ee30aef42cd0087
mrHock89 <147297458+mrHock89@users.noreply.github.com>
2025-01-03 11:37:50 +0530

    solved exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function (myString) {
+  let mySplitString = myString.split("");
+  const reverseArr = mySplitString.reverse();
+  return reverseArr.join("");
 };
 
 // Do not edit below this line
